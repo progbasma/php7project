71d7fdba216f9f7625c132804f99004e0b5e0dda
basma <prog.basma@gmail.com>
2021-07-02 13:58:40 +0200

    b

--- about.php
+++ about.php
@@ -303,25 +303,3 @@ include('includes/header.php');
 include('includes/footer.php');
 ?>
 <!-- footer area end -->
-
-
-
-
-<script src="js/vendor/jquery-1.10.2.min.js"></script>
-<script src="js/bootstrap.min.js"></script>
-<script src="js/smoothscroll.js"></script>
-<!-- Scroll up js
-============================================ -->
-<script src="js/jquery.scrollUp.js"></script>
-<script src="js/menu.js"></script>
-<script src="js/owl.carousel.min.js"></script>
-<script src="js/jquery.collapse.js"></script>
-
-<script type="text/javascript" src="assets/plugins/counter/waypoints.min.js"></script>
-<script type="text/javascript" src="assets/plugins/counter/jquery.counterup.min.js"></script>
-<script type="text/javascript" src="js/counters.js"></script>
-
-<script src="js/main.js"></script>
-</body>
-
-</html>
